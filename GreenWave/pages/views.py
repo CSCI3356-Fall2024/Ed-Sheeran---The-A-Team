976088ce5fa2adca1530769f46100708f3be129c
LukeJG <luke.gearin5@gmail.com>
2024-10-21 20:00:26 -0400

    make only one profile / update profile

--- GreenWave/pages/views.py
+++ GreenWave/pages/views.py
@@ -10,15 +10,23 @@ def home_view(request, *args, **kwargs):
     return render(request, "home.html", {})
 
 def profile_view(request):
+    user_profile_instance = user_profile.objects.filter(user=request.user).first()
+
     if request.method == 'POST':
         form = profile_form(request.POST, request.FILES)
         if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = request.user
-            profile.save()
-            print("Profile saved:", profile)  # Log the saved profile
-        else:
-            print("Form errors:", form.errors)
+            if user_profile_instance:
+                profile = form.save(commit=False)
+                profile.user = request.user
+                profile.id = user_profile_instance.id
+                profile.save()
+
+
+            else:
+                profile = form.save(commit=False)
+                profile.user = request.user
+                profile.save()
+
         #    image = form.cleaned_data["image"]
         #    school = form.cleaned_data["school"]
         #    year = form.cleaned_data["year"]
@@ -27,7 +35,7 @@ def profile_view(request):
         #    t = user_profile(school=school, year=year, major1=major1, major2=major2 )
         #    t.save()
     else:
-        form = profile_form()
+        form = profile_form(instance=user_profile_instance)
 
 
-    return render(request, "profile.html", {'form':form})
+    return render(request, "profile.html", {'form':form, 'user_profile': user_profile_instance})
