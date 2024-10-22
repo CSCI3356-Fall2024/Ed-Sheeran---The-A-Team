ad512d94111736ebb49632f4833e0073b5b885ba
HannahRoche <rochehb@bc.edu>
2024-10-22 00:21:25 -0400

    Dropdown list for year, more HTML style updates
    Changed styling of base and profile htmls, also changed the format of the year on the form to a dropdown.

--- GreenWave/pages/forms.py
+++ GreenWave/pages/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import user_profile
 
 class profile_form(forms.ModelForm):
+    YEAR_CHOICES = [(2025, '2025'),(2026, '2026'),(2027, '2027'),(2028, '2028')]
+    year = forms.ChoiceField(choices=YEAR_CHOICES)
+
     class Meta:
         model = user_profile
         fields = ["image", "school", "year", "major1", "major2"]
@@ -11,5 +14,3 @@ class profile_form(forms.ModelForm):
         #    'major1': forms.Select(attrs={'class': 'form-control'}),
         #    'major2': forms.Select(attrs={'class': 'form-control'})
         #}
-
-
