259589f0d1d1d2852ac66a8df99bbf5b7a59326a
baldari <baldarimatt@gmail.com>
2024-10-30 10:45:34 -0400

    fixed dropdowns in profile and campaign forms, started formatting campaigns form with css

--- GreenWave/pages/admin.py
+++ GreenWave/pages/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import user_profile
 from .models import campaign
+from .models import service
 # Register your models here.
 
 admin.site.register(user_profile)
 admin.site.register(campaign)
+admin.site.register(service)
--- GreenWave/pages/forms.py
+++ GreenWave/pages/forms.py
@@ -6,8 +6,8 @@ from .models import service
 class profile_form(forms.ModelForm):
     YEAR_CHOICES = [(2025, '2025'),(2026, '2026'),(2027, '2027'),(2028, '2028')]
     year = forms.ChoiceField(choices=YEAR_CHOICES)
-    #SCHOOL_CHOICES = ["Boston College"] commented out but thought we should add?
-    #school = forms.ChoiceField(choices = SCHOOL_CHOICES)
+    SCHOOL_CHOICES = [('Boston College', 'Boston College')]
+    school = forms.ChoiceField(choices = SCHOOL_CHOICES)
 
     class Meta:
         model = user_profile
@@ -19,6 +19,8 @@ class profile_form(forms.ModelForm):
         #    'major2': forms.Select(attrs={'class': 'form-control'})
         #}
 class campaign_form(forms.ModelForm):
+    VALIDATION_CHOICES = [('Choice 1', 'Choice 1'), ('Choice 2', 'Choice 2')]
+    validation = forms.ChoiceField(choices = VALIDATION_CHOICES)
     class Meta:
         model = campaign
         fields = ["name", "start_date", "end_date", "points", "validation", "description"]
